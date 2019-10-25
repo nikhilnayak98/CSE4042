6ada2304641b340ac82f84e3c23744da658505d5
Nikhil Nayak <nikhilnayak98@gmail.com>
2019-10-25 17:41:52 +0530

    serv cli dc

---
+++ DebitCard/client_debit_card.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+
+int main(int argc, char *argv[]) {
+    int clien_fd, bind_result, read_result;
+    struct sockaddr_in serve_addr;
+}
